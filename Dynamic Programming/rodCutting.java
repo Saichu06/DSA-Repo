1868cfc028df4cddb9036803b1e9a727daef85cb
Saichu <assaicharan06@gmail.com>
2026-06-05 09:08:42 +0530

    rod cutting

---
+++ Dynamic Programming/rodCutting.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class rodCutting {
+    public int cutRod(int[] price) {
+        // code here
+        int n = price.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int i = 1 ; i<=n ; i++){
+            for(int j = 1 ; j<=n ; j++){
+                if(j>=i){
+                    dp[i][j]=Math.max(dp[i-1][j] , price[i-1]+dp[i][j-i]);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][n];
+    }
+}

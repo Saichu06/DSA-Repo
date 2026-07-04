ffb674a9cbbfdd6b8875d42332e1df3231c2f064
Saichu <assaicharan06@gmail.com>
2026-07-04 14:51:04 +0530

    Last stone weight Amazon AC DSA - easy complete

---
+++ Apna College sheet/Amazon/palindomeLL.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class palindomeLL {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        while (temp != null) {
+            list.add(temp.val);
+            temp = temp.next;
+        }
+        int n = list.size();
+        for (int i = 0; i < n; i++) {
+            if (list.get(i) != list.get(n - i - 1))
+                return false;
+        }
+        return true;
+    }
+}

f0c93d3a3910188219c01d735cb7de4c9a6f890d
Saicharan <assaicharan06@gmail.com>
2026-07-21 16:52:39 +0530

    Microsoft med - LL Cycle , Generate () , Reverse LL , Substring of 1 , number of 1 bits

---
+++ Apna College sheet/Microsoft/Medium/generateparanthesis.java
@@ -0,0 +1,27 @@
+class generateparanthesis {
+    public ArrayList<String> generateParentheses(int n) {
+        // code here
+        ArrayList<String> ans = new ArrayList<>();
+        int openCount=0;
+        int closeCount=0;
+        String s="";
+        n=n/2;
+        helper(ans , openCount , closeCount , s , n);
+        return ans;
+    }
+    
+    public void helper(List<String> ans , int open , int close , String s , int n){
+        if (open == n && close == n) {
+    ans.add(s);
+    return;
+}
+
+if (open > close) {
+    helper(ans, open, close + 1, s + ")", n);
+}
+
+if (open < n) {
+    helper(ans, open + 1, close, s + "(", n);
+}
+    }
+}
---
+++ Apna College sheet/Microsoft/Medium/numberof1bits.java
@@ -0,0 +1,11 @@
+class numberof1bits {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n>0){
+            if((n&1)!=0) count++;
+            n=n>>1;
+        }
+
+        return count;
+    }
+}

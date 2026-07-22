8518f2ccf9604238e6551d7aae76365fc7f759c5
Saicharan <assaicharan06@gmail.com>
2026-07-22 15:29:23 +0530

    Microsoft med - Validate stack sequences

---
+++ Apna College sheet/Microsoft/Medium/validatestacksequences.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class validatestacksequences {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st = new Stack<>();
+        int n=pushed.length;
+        int j=0;
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && popped[j]==st.peek()){
+                st.pop();
+                j++;
+            }
+            st.push(pushed[i]);
+        }   
+
+        while(!st.isEmpty() && popped[j]==st.peek()){
+                st.pop();
+                j++;
+        }
+
+        return st.isEmpty() && j==n;
+
+    }
+}

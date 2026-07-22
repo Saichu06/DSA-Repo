76c1f31bb14e45a512654ec6024f549a4be2dc70
Saicharan <assaicharan06@gmail.com>
2026-07-22 10:26:36 +0530

    MS - Medium Delete a node , Longest substring

---
+++ Apna College sheet/Microsoft/Medium/longestsubstring.java
@@ -0,0 +1,17 @@
+class longestsubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int maxlen=0;
+        Set<Character> set = new HashSet<>();
+        int left=0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            while(set.contains(c)){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            maxlen=Math.max(maxlen , i-left+1);
+            set.add(c);
+        }
+        return maxlen;
+    }
+}

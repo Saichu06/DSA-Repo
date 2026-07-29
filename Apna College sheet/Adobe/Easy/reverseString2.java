3e23689020a4fd54e300934af24e9eea03861464
Saicharan <assaicharan06@gmail.com>
2026-07-29 10:00:04 +0530

    Min Index Sum Two lists : Adobe , ReverseString 2

---
+++ Apna College sheet/Adobe/Easy/reverseString2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class reverseString2 {
+     public String reverseStr(String s, int k) {
+        char[] str = s.toCharArray();
+        for(int i=0;i<s.length();i+=2*k){
+            int left=i;
+            int right = Math.min(i + k - 1, s.length() - 1);
+            while(left<right){
+                char temp = str[left];
+                str[left] = str[right];
+                str[right]=temp;
+                left++;
+                right--;
+            }
+        }
+
+        return String.valueOf(str);
+    }
+}

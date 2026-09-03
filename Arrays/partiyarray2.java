fb0559c1080aaa3d51407b2561868dd8e931ca7f
Saicharan <assaicharan06@gmail.com>
2026-09-03 17:31:15 +0530

    parity array 2

---
+++ Arrays/partiyarray2.java
@@ -0,0 +1,20 @@
+public class parityarray2 {
+    public boolean uniformArray(int[] nums1) {
+        int min = Integer.MAX_VALUE;
+        for (int num : nums1) {
+            min = Math.min(min, num);
+        }
+
+        if (min % 2 != 0) {
+            return true;
+        }
+
+        for (int num : nums1) {
+            if (num % 2 != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

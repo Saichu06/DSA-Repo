58bf133a5cf4ea9c966d0dc34d032d23fead9188
Saicharan <assaicharan06@gmail.com>
2026-09-09 09:38:37 +0530

    Count commas 2

---
+++ Arrays/countCommas2.java
@@ -0,0 +1,12 @@
+public class countCommas2{
+    public long countCommas(long n) {
+        long x = 1000;
+        long ans = 0;
+        while(x<=n){
+            ans+=(n-x+1);
+            x*=1000;
+        }
+
+        return ans;
+    }
+}
